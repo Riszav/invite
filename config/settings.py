cfceb749513b87b86c6f92c0bc0ac1144d791f73
root <root@vmi2183831.contaboserver.net>
2025-07-28 13:41:47 +0200

    changes from server

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-rucdvawefvitv203qg0oyofo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['109.123.253.64']
 
 
 # Application definition
